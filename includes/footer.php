3bc57281cea8a775f3750f576318a733c75c269a
solankikeval166 <solankikeval166@gmail.com>
2023-09-04 21:26:17 +0530

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<section class="footer-section footer">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Book World &copy; <?php echo date('Y'); ?></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</section>
